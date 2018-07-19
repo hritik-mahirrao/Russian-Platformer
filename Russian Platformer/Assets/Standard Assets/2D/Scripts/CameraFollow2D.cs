362eda1b36cfb97429114d70c4e74f4b921eb175
Nikhil Mahirrao <nikhil.mahirrao@gmail.com>
2018-07-19 20:53:58 +0530

    New Camera follow 2D script added

---
+++ Russian Platformer/Assets/Standard Assets/2D/Scripts/CameraFollow2D.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour {
+
+	public Transform LookAt;
+
+	public float boundX = 2.0f;
+	public float boundY = 1.0f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void LateUpdate() {
+		Vector3 delta = Vector3.zero;
+
+		// X Axis
+		float dx = LookAt.position.x - transform.position.x;
+			
+		if(dx > boundX || dx < -boundX) {
+
+			if(transform.position.x < LookAt.position.x) {
+				delta.x = dx - boundX;
+			} else {
+				delta.x = dx + boundX;
+			}
+		}
+
+		// Y Axis
+		float dy = LookAt.position.y - transform.position.y;
+			
+		if(dy > boundY || dy < -boundY) {
+
+			if(transform.position.y < LookAt.position.y) {
+				delta.y = dy - boundY;
+			} else {
+				delta.y = dy + boundY;
+			}
+		}
+
+		// Move camera
+		transform.position = transform.position + delta;
+	}
+}
