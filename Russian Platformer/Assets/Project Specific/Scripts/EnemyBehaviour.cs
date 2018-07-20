8d69681845dd63b4aa29d4fa992b37c4ce3cbf27
Hritik Narendra <hritiknarendra@gmail.com>
2018-07-20 21:37:14 +0530

    Enemy Animations integrated

--- Russian Platformer/Assets/Project Specific/Scripts/EnemyBehaviour.cs
+++ Russian Platformer/Assets/Project Specific/Scripts/EnemyBehaviour.cs
@@ -15,6 +15,11 @@ public class EnemyBehaviour : MonoBehaviour
 
 
     private Rigidbody2D enemyRigidBody2D;
+    private Animator enemyAnimatorController;
+
+
+    private bool m_FacingRight = true;  // For determining which way the enemy is currently facing.
+
 
     public enum Enemy_States
     {
@@ -34,6 +39,7 @@ public class EnemyBehaviour : MonoBehaviour
     {
         Player = GameObject.Find("CharacterRobotBoy").GetComponent<Transform>();
         enemyRigidBody2D = GetComponent<Rigidbody2D>();
+        enemyAnimatorController = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -78,8 +84,10 @@ public class EnemyBehaviour : MonoBehaviour
                 currentWayPoint = 0;
             }
 
-            transform.position = Vector2.MoveTowards(transform.position, Patrol_Points[currentWayPoint].transform.position, 0.3f);
+            enemyRigidBody2D.velocity = new Vector2(moveSpeed * maxSpeed, enemyRigidBody2D.velocity.y);
 
+            //transform.position = Vector2.MoveTowards(transform.position, new Vector2(Patrol_Points[currentWayPoint].transform.position.x, transform.position.y), Time.deltaTime * moveSpeed);
+            enemyAnimatorController.SetFloat("Speed", Mathf.Abs(moveSpeed));
 
             //for (int i = 0; i < Patrol_Points.Length - 1; i++)
             //{
@@ -89,8 +97,34 @@ public class EnemyBehaviour : MonoBehaviour
             //    }
             //}
 
+            // If the input is moving the enemy right and the player is facing left...
+            if (moveSpeed > 0 && !m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+            // Otherwise if the input is moving the enemy left and the enemy is facing right...
+            else if (moveSpeed < 0 && m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+
         }
     }
+
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
 
     void OnDrawGizmos()
     {
