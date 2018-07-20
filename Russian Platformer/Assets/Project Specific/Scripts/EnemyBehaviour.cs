d443c202b8236f4898fdd4f086aa085c4a50230e
Nikhil Mahirrao <nikhil.mahirrao@gmail.com>
2018-07-20 21:21:38 +0530

    Basic skeleton for enemy movement added

--- Russian Platformer/Assets/Project Specific/Scripts/EnemyBehaviour.cs
+++ Russian Platformer/Assets/Project Specific/Scripts/EnemyBehaviour.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    public Transform Player;
-    public Transform[] Patrol_Points;
+    // Game Object
+    public Transform Hero;
 
-    public float maxSpeed;
-    public float moveSpeed;
-    int currentWayPoint = 0;
+    // State change distances
+    public float alertDistance;
+    public float walkDistance;
+    public float meleDistance;
 
-    public float lookRadiusOuter, lookRadiusInner;
+    public float runSpeed;
+    public float walkSpeed;
 
-
-    private Rigidbody2D enemyRigidBody2D;
-
+    // States
+    public Enemy_States enemy_state;
     public enum Enemy_States
     {
         //Idle,
@@ -27,8 +28,9 @@ public class EnemyBehaviour : MonoBehaviour
         Fallen
     };
 
-    public Enemy_States enemy_state;
-
+    private Rigidbody2D enemyRigidBody2D;
+    private bool isAlert = false;
+    
     // Use this for initialization
     void Start()
     {
@@ -39,10 +41,24 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Enemy_State_Management();
-        Enemy_Movement();
+        updatePlayer();
     }
 
+    void updatePlayer() {
+
+        Vector2 distanceFromPlayer = Vector2.Distance(transform.position, Player.transform.position);
+        if (!isAlert && distanceFromPlayer.x < alertDistance) {
+            // Alert the enemy
+            isAlert = true;
+        } else if (distanceFromPlayer.x <= meleDistance) {
+            // Trans position
+            // Mele combact
+        } else if (distanceFromPlayer.x <= walkDistance) {
+            // Start walking towards Hero
+        } else {
+            // Run towards Hero
+        }
+    }
 
     void Enemy_State_Management()
     {
@@ -68,17 +84,17 @@ public class EnemyBehaviour : MonoBehaviour
         if (enemy_state == Enemy_States.Patrol)
         {
 
-            if (Vector2.Distance(transform.position, Patrol_Points[currentWayPoint].transform.position) < 2)
-            {
-                currentWayPoint++;
-            }
+            // if (Vector2.Distance(transform.position, Patrol_Points[currentWayPoint].transform.position) < 2)
+            // {
+            //     currentWayPoint++;
+            // }
 
-            if (currentWayPoint >= Patrol_Points.Length - 1)
-            {
-                currentWayPoint = 0;
-            }
+            // if (currentWayPoint >= Patrol_Points.Length - 1)
+            // {
+            //     currentWayPoint = 0;
+            // }
 
-            transform.position = Vector2.MoveTowards(transform.position, Patrol_Points[currentWayPoint].transform.position, 0.3f);
+            // transform.position = Vector2.MoveTowards(transform.position, Patrol_Points[currentWayPoint].transform.position, 0.3f);
 
 
             //for (int i = 0; i < Patrol_Points.Length - 1; i++)
