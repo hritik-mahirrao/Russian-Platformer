09d6b394ffe51072af721473ca7883cd9cbc232a
Nikhil Mahirrao <nikhil.mahirrao@gmail.com>
2018-07-21 16:38:38 +0530

    Camera Clamp added

--- Russian Platformer/Assets/Standard Assets/2D/Scripts/CameraFollow2D.cs
+++ Russian Platformer/Assets/Standard Assets/2D/Scripts/CameraFollow2D.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class CameraFollow2D : MonoBehaviour {
 
 	public Transform LookAt;
+	public GameObject LeftBound;
+	public GameObject RightBound;
 
 	public float boundX = 2.0f;
 	public float boundY = 1.0f;
 	
+
 	// Use this for initialization
 	void Start () {
 		
@@ -20,6 +23,7 @@ public class CameraFollow2D : MonoBehaviour {
 	}
 
 	private void LateUpdate() {
+
 		Vector3 delta = Vector3.zero;
 
 		// X Axis
@@ -34,19 +38,27 @@ public class CameraFollow2D : MonoBehaviour {
 			}
 		}
 
-        //// Y Axis
-        //float dy = LookAt.position.y - transform.position.y;
-			
-        //if(dy > boundY || dy < -boundY) {
+		// Y Axis
+		//float dy = LookAt.position.y - transform.position.y;
+	
+		//if(dy > boundY || dy < -boundY) {
 
-        //    if(transform.position.y < LookAt.position.y) {
-        //        delta.y = dy - boundY;
-        //    } else {
-        //        delta.y = dy + boundY;
-        //    }
-        //}
+		//    if(transform.position.y < LookAt.position.y) {
+		//        delta.y = dy - boundY;
+		//    } else {
+		//        delta.y = dy + boundY;
+		//    }
+		//}
 
 		// Move camera
 		transform.position = transform.position + delta;
+
+		// Clamp camera
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x, LeftBound.transform.position.x, RightBound.transform.position.x),
+			Mathf.Clamp(transform.position.y, LeftBound.transform.position.y, RightBound.transform.position.y),
+			Mathf.Clamp(transform.position.z, LeftBound.transform.position.z, RightBound.transform.position.z)
+		);
+
 	}
 }
