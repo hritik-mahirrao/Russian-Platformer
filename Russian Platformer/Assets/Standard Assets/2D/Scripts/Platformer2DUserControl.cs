dcf03d4d1df74169bcbb20a924139bd9e918fbc1
Hritik Narendra <hritiknarendra@gmail.com>
2018-07-22 15:33:09 +0530

    Complete player integrated

--- Russian Platformer/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Russian Platformer/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -4,12 +4,13 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets._2D
 {
-    [RequireComponent(typeof (PlatformerCharacter2D))]
+    [RequireComponent(typeof(PlatformerCharacter2D))]
     public class Platformer2DUserControl : MonoBehaviour
     {
         private PlatformerCharacter2D m_Character;
         private bool m_Jump;
-
+        private bool m_Knife;
+        private bool m_Kick;
 
         private void Awake()
         {
@@ -23,6 +24,8 @@ namespace UnityStandardAssets._2D
             {
                 // Read the jump input in Update so button presses aren't missed.
                 m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                m_Knife = CrossPlatformInputManager.GetButtonDown("Knife");
+                m_Kick = CrossPlatformInputManager.GetButtonDown("Kick");
             }
         }
 
@@ -33,8 +36,11 @@ namespace UnityStandardAssets._2D
             bool crouch = Input.GetKey(KeyCode.LeftControl);
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             // Pass all parameters to the character control script.
-            m_Character.Move(h, crouch, m_Jump);
+            m_Character.Move(h, crouch, m_Jump, m_Knife, m_Kick);
             m_Jump = false;
+            m_Knife = false;
+            m_Kick = false;
+
         }
     }
 }
